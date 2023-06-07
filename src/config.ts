b95096c482d5b413f5f4439ef5a26c751b774e11
Fredys M. Borges <mb.freddys@gmail.com>
2023-06-06 23:56:36 -0400

    cambiando el tema de la pagina

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Fredys Matos Borges | Personal Website';
+export const SITE_TITLE = 'Fredys Matos Borges | Website';
 export const SITE_DESCRIPTION = 'Automation & Control Engineer | Hardware Designer (PCB) | IoT Apprentice | Passionate about Robotics';
